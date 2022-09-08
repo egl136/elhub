14626bcd590403fce8c434d05ea1b6f96a0c941f
lalo2 <eduardoguevaralozano1@gmail.com>
2022-09-07 22:41:22 -0500

    index routes added

--- public/app.js
+++ public/app.js
@@ -1,1 +1,43 @@
-Vue.createApp({}).mount("#app")
+const vm = Vue.createApp({
+    data(){
+        return {
+            mail:string = 'John',
+            passwd:string = 'Loa',
+            loginStatusText:string = "Not logged yet",
+            loginStatus:boolean = false,
+            data:json = {},
+            contentIndex:number = 0
+        }
+    },
+    methods:{
+        /*fullName(){
+            return 'Email: ' + this.mail + ' Passwd: ' + this.passwd
+        },*/
+        async login(){
+            this.loginStatusText = "Loggin In..."
+            let mail = this.mail
+            let passwd = this.passwd
+            let _post = {
+                usuinput: mail,
+                pasinput: passwd
+            }
+            console.log(JSON.stringify(_post))
+            let response = await fetch('login/action', {
+                method: "POST",
+                body: JSON.stringify(_post),
+                headers: {'Content-type': 'application/json'}
+            })
+            const responseText = await response.json().then(jsonR => this.json=jsonR)
+            if(this.json.failed){
+                this.loginStatusText = this.json.error_msg
+            }else{
+                this.loginStatusText = "Bienvenido, "+this.json.user.name
+            }
+        }
+    }
+}).mount("#app")
+
+
+
+
+
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -16,24 +16,83 @@ const connection = mysql.createConnection({
     database: 'TxaExXMhdw'
 })
 
-const login = (req, res)=>{
+const login = async (req, res)=>{
     //Get post from request
     const mail = req.body.usuinput
     const psswd = req.body.pasinput
     //Connect to database
-    connection.connect()
+    //await connection.connect()
     const query = "SELECT * FROM Users WHERE user_mail = '"+mail+"'"
+    console.log(query)
     let hash = ""
+    var respuesta = {
+        failed: true,
+        error_msg: "",
+        user: {
+            id: "",
+            mail: "",
+            name: "",
+            lname: "",
+            tipo: 0,
+            telefono: "",
+            sexo: "",
+            empresa: "",
+            rfc: "",
+            pais: "",
+            ciudad: ""
+        }
+    }
     connection.query(query, (err, results, fields) => {
-        if (err) res.send(err)
-        if (results.length!==1) res.send(false)
+        if (err) respuesta.error_msg = err
+        if (results.length!==1) {
+            respuesta.error_msg = "Correo no encontrado"
+            console.log(JSON.stringify(respuesta))
+            res.json(respuesta)
+        }
         //As hashes were generated with php, they use $2y$ prefix, so it is
         //just exchanged to $2a$ prefix
-        hash = results[0].user_hash.replace('$2y$', '$2a$')
-        //Sends comparation result
-        res.send(bcrypt.compareSync(psswd, hash))
+        else{
+            hash = results[0].user_hash.replace('$2y$', '$2a$')
+            //Validates passwd with hashed one
+            let matched = bcrypt.compareSync(psswd, hash)
+            if(matched){
+                respuesta.user.id = results[0].user_id
+                respuesta.user.mail = results[0].user_mail
+                respuesta.user.telefono = results[0].user_tel
+                respuesta.user.tipo = results[0].user_type
+                respuesta.user.name = results[0].user_name
+                respuesta.user.lname = results[0].user_lname
+                console.log(respuesta.user.mail)
+                let table = "Normal"
+                if(results[0].tipo) table = "Empresas"
+                const getUser = "SELECT * FROM "+table+" WHERE user_id='"+results[0].user_id+"'"
+                console.log(getUser)
+                connection.query(getUser, (err, resu, fields) => {
+                    if (err) console.log(err)
+                    if (resu.length!==1) respuesta.error_msg = "Error con la integridad de los datos."
+                    else{
+                        if(table === "Normal"){
+                            respuesta.user.sexo = resu[0].sexo
+                            console.log(respuesta.user.sexo)
+                        }else{
+                            respuesta.user.empresa = resu[0].emp_id
+                            respuesta.user.rfc = resu[0].emp_rfc
+                            respuesta.user.pais = resu[0].emp_country
+                            respuesta.user.ciudad = resu[0].emp_city
+                        }
+                        respuesta.failed = false
+                        res.json(respuesta)
+                        console.log(JSON.stringify(respuesta))
+                    }
+                })
+            }else{
+                respuesta.error_msg = "Contraseña Incorrecta"
+                res.json(respuesta)
+            }
+        }
     })
-    connection.end()
+    //res.json(respuesta)
+    //connection.end()
 }
 
 console.log("Desde el controlador de login")
